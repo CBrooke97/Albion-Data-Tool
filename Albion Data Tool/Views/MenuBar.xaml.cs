43ddd308e117f1a6e2cd0cf6474ff405069f2575
Carter Brooke <carterbrooke@ntlworld.com>
2025-04-23 18:35:22 +0100

    Init commit

---
+++ Albion Data Tool/Views/MenuBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Albion_Data_Tool.Views;
+
+public partial class MenuBar : UserControl
+{
+    public MenuBar()
+    {
+        InitializeComponent();
+    }
+}
