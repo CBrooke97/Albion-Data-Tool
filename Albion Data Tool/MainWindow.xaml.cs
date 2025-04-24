17869ca685613f4c055985f6374df87a1f758201
AcademyCarter <cbrooke@sumo-digital.com>
2025-04-24 13:34:33 +0100

    Started investigating HttpClient using API calls and deserializing JSON

--- Albion Data Tool/MainWindow.xaml.cs
+++ Albion Data Tool/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Albion_Data_Tool;
 
@@ -19,5 +22,62 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        _ = LoadDataAsync();
+    }
+
+    private async Task LoadDataAsync()
+    {
+        HttpClient client = new HttpClient();
+        string json = await client.GetStringAsync($"https://east.albion-online-data.com/api/v2/stats/prices/T4_BAG,T5_BAG.json");
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        AlbionMarketEntry[] MarketEntry = JsonSerializer.Deserialize<AlbionMarketEntry[]>(json, options);
+
+        foreach(var Entry in MarketEntry)
+        {
+            Console.WriteLine($"City: {Entry.City}, Min Sell Price: {Entry.SellPriceMin}");
+        }
     }
 }
+
+public class AlbionMarketEntry
+{
+    [JsonPropertyName("item_id")]
+    public string ItemId { get; set; }
+
+    [JsonPropertyName("city")]
+    public string City { get; set; }
+
+    [JsonPropertyName("quality")]
+    public int Quality { get; set; }
+
+    [JsonPropertyName("sell_price_min")]
+    public int SellPriceMin { get; set; }
+
+    [JsonPropertyName("sell_price_min_date")]
+    public DateTime SellPriceMinDate { get; set; }
+
+    [JsonPropertyName("sell_price_max")]
+    public int SellPriceMax { get; set; }
+
+    [JsonPropertyName("sell_price_max_date")]
+    public DateTime SellPriceMaxDate { get; set; }
+
+    [JsonPropertyName("buy_price_min")]
+    public int BuyPriceMin { get; set; }
+
+    [JsonPropertyName("buy_price_min_date")]
+    public DateTime BuyPriceMinDate { get; set; }
+
+    [JsonPropertyName("buy_price_max")]
+    public int BuyPriceMax { get; set; }
+
+    [JsonPropertyName("buy_price_max_date")]
+    public DateTime BuyPriceMaxDate { get; set; }
+}
+
